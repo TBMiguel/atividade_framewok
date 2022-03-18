189a54178f04dbd26ec0097029e210f878074e96
Your Name <miguel.junior@alunos.unicesumar.edu.br>
2022-03-17 21:25:54 -0300

    Atividade Framework Livro

---
+++ database/migrations/1647561672124_livros.ts
@@ -0,0 +1,21 @@
+import BaseSchema from '@ioc:Adonis/Lucid/Schema'
+
+export default class Livros extends BaseSchema {
+  protected tableName = 'livros'
+
+  public async up () {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id')
+      table.string('titulo')
+      table.string('genero')
+      table.string('autor')
+      table.date('anoMes')
+      table.integer('classficacao')
+      table.text('resumo', 'longtext')
+    })
+  }
+
+  public async down () {
+    this.schema.dropTable(this.tableName)
+  }
+}
